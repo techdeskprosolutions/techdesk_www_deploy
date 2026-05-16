399d1d04bf9978610c4b1e1511dff593860ca677
techdeskprosolutions <techdeskprosolutions@gmail.com>
2026-05-16 02:29:05 -0400

    Update config.php

--- private/config.php
+++ private/config.php
@@ -10,5 +10,5 @@ try {
     $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8mb4", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    die("Database connection failed: " . $e->getMessage());
+    die("Database connection failed.");
 }
